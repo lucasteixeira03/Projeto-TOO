d6009aed5ad0e1ab477c87f44e3dcda5a805f7dc
lucasteixeira03 <lucassteixeira2003@hotmail.com>
2025-10-22 11:34:55 -0300

    Atividade 22-10-25

--- model/TarefaGamer.py
+++ model/TarefaGamer.py
@@ -2,12 +2,57 @@ from .Tarefa import Tarefa
 from datetime import datetime
 
 class TarefaGamer(Tarefa):
-    def __init__(self, titulo, tipo=None, descricao=None, data_realizacao=None):
-        super().__init__(titulo, descricao=descricao, data_realizacao=data_realizacao)
-        self.tipo = tipo  # Exemplo: "Saúde", "Estudos", "Lazer" etc.
+    def __init__(self, nome_tarefa, jogo: str, nivel=0, descricao=None, data_realizaca=None, data_limite=None):
+        super().__init__(nome_tarefa, descricao, data_realizaca)
+        self.jogo = jogo
+        self.nivel = nivel
+        self.__data_limite = None
+        if data_limite is not None:
+            self.data_limite = data_limite
+
+    def __str__(self):
+        return f"[Tarefa Gamer] {super().__str__()}"
+
+    @property
+    def jogo(self) -> str:
+        return self.__jogo
+
+    @jogo.setter
+    def jogo(self, nome_jogo: str):
+        self.__jogo = nome_jogo
+
+    @property
+    def nivel(self):
+        return self.__nivel
+
+    @nivel.setter
+    def nivel(self, valor):
+        self.__nivel = valor
+
+    @property
+    def data_limite(self):
+        return self.__data_limite
+
+    @data_limite.setter
+    def data_limite(self, str_data_limite):
+        self.__data_limite = None
+        if str_data_limite is not None:
+            try:
+                self.__data_limite = datetime.strptime(str_data_limite, "%d-%m-%Y")
+            except ValueError as e:
+                print(f"Erro ao converter \"{str_data_limite}\": {e}")
+
+    def exibir_dados(self):
+        data_limite = f"{self.__data_limite.strftime('%d-%m-%Y')}" if self.data_limite is not None else "Sem data definida"
+        txt = f"{super().exibir_dados()} \n Data Limite: {data_limite}"
+        if self.jogo:
+            txt += f"\n Jogo: {self.jogo}"
+        if self.nivel != 0:
+            txt += f"\n Nível: {self.nivel}"
+        return txt
 
     def definir_termino(self):
         self.data_realizacao = datetime.now()
-
-    def exibir_dados(self):
-        return super().exibir_dados()
+        if self.data_limite is not None and self.data_realizacao > self.data_limite:
+            str_desc = f"{self.descricao} [Concluída em atraso]" if self.descricao else "[Concluída em atraso]"
+            self.descricao = str_desc
--- teste_gamer.py
+++ teste_gamer.py
@@ -1,8 +1,13 @@
 from model.TarefaGamer import TarefaGamer
 from datetime import datetime
 
-
-a = TarefaGamer(titulo="Projeto Teste")
+a = TarefaGamer(
+    nome_tarefa="Zerar Elden Ring",
+    jogo="Elden Ring",
+    nivel=50,
+    descricao="Finalizar o último chefe",
+    data_limite="25-10-2025"
+)
 
 print(a)
 print(a.exibir_dados())
